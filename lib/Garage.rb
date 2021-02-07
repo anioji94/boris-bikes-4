94f4a5ac9e4ea4eb39ebc2309eb5de1229223062
Aniruddh <ojhaa94@gmail.com>
2021-02-07 16:57:34 +0000

    Garages can now fix multiple bikes at the same time

--- lib/Garage.rb
+++ lib/Garage.rb
@@ -1,5 +1,21 @@
 class Garage 
+	attr_reader :hold 
+	
+	def initialize
+		@hold = []
+	end
+
 	def fix_bike(bike)
 		bike.fix
 	end
+
+
+	def fix_all_bikes
+
+		@hold.map do |bike|
+			fix_bike(bike)
+		end
+
+	end
+
 end
--- spec/Garage_spec.rb
+++ spec/Garage_spec.rb
@@ -14,4 +14,19 @@ describe Garage do
 			expect(bike.broken).to be false
 		end
 	end
+
+	describe '#fix_all_bikes' do 
+		it { is_expected.to respond_to(:fix_all_bikes) }
+
+		it 'should fix all the bikes held in the Garage' do 
+			bike = Bike.new
+			bike.report
+			bike2 = Bike.new
+			bike2.report 
+			subject.hold << bike
+			subject.hold << bike2
+			subject.fix_all_bikes
+			expect(subject.hold[0].broken).to be false
+		end
+	end
 end
